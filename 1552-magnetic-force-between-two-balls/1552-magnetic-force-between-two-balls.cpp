0820b75cd06a08b1859a7a82127037c5b1acf5a3
Pranvi Singla <drishtisingla.rmn@gmail.com>
2026-08-09 17:34:11 +0530

    Time: 43 ms (88.67%), Space: 61.6 MB (96.36%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int maxDistance(vector<int>& position, int m) {
-        int n=position.size()-1;
+        int n=position.size();
         long long st=1;
         sort(position.begin(),position.end());
-        long long end=position[n]-position[0];
+        long long end=position[n-1]-position[0];
 
         while(st<=end){
             long long mid=st+(end-st)/2;
 
             int ballsPlaced=1;
             int lastPos=position[0];
-            for(int i=1;i<=n;i++){
+            for(int i=1;i<n;i++){
                 if((position[i]-lastPos)>=mid){
                     ballsPlaced++;
                     lastPos=position[i];
